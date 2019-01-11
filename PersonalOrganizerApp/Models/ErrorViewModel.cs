c25db6db170e2f19a0074d6955fd86346349c7a6
Ryan Haire <Ryan@RyansMac.local>
2019-01-11 12:47:09 -0500

    First commit

---
+++ PersonalOrganizerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PersonalOrganizerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
